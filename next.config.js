178e71684b3d1d75519fbf7f813325ed476d7e19
Snorre Fossland <snorres@gmail.com>
2025-09-01 09:33:46 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
     },
     // Remove conflicting experimental options
     transpilePackages: ['class-variance-authority'],
+    eslint: {
+        // Warning: this will skip lint checks during build on Vercel
+        ignoreDuringBuilds: true
+    }
 }
 
 module.exports = nextConfig;
